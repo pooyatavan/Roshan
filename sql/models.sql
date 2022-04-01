03bccec73d6d42175acfb64a75087e4a5c983a45
Pooya <91305229+pooyatavan@users.noreply.github.com>
2022-04-01 09:02:50 +0430

    Create models.sql

---
+++ sql/models.sql
@@ -0,0 +1,12 @@
+/*
+SQLyog Ultimate
+MySQL - 8.0.28 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `models` (
+	`id` bigint (20),
+	`models` varchar (300),
+	`os` varchar (300)
+); 
