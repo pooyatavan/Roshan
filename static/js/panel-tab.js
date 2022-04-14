7b09d5187a6d32a067f49ccd2bb791adf782f25a
Admiral <91305229+pooyatavan@users.noreply.github.com>
2022-04-15 00:40:48 +0430

    Add files via upload

--- static/js/panel-tab.js
+++ static/js/panel-tab.js
@@ -1,14 +1,13 @@
 function openPage(evt, pageName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(pageName).style.display = "block";
-    evt.currentTarget.className += " active";
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(pageName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
-document.getElementById("defaultOpen").click();
--- static/js/solar.js
+++ static/js/solar.js
@@ -4,105 +4,102 @@ var y = 0;
 var m = 0;
 var t = []
 var x = []
-
-function copy(that){
-    var inp =document.createElement('input');
-    document.body.appendChild(inp)
-    inp.value =that.textContent
-    inp.select();
-    document.execCommand('copy',false);
-    inp.remove();
-}
-
-function update_values() {
-    $.getJSON($SCRIPT_ROOT + '/_stuff', function(data) {
-        x = document.getElementsByClassName("right");
-        j = 0;
-        i = 0;
-        for (m = 0; m < data.alldata[2].length; m++) {
-            if (m == data.alldata[2].length) {
-                break;
-            } else {
-                document.getElementById(x[m].id).innerHTML = "";
-                document.getElementById(t[m].id).innerHTML = "";
-            }
+function update_values() {$.getJSON($SCRIPT_ROOT + '/_stuff', function(data) {
+    x = document.getElementsByClassName("right");
+    j = 0;
+    i = 0;
+    for (m = 0; m < data.alldata[2].length; m++) {
+        if (m == data.alldata[2].length) {
+            break;
+        } else {
+            document.getElementById(x[m].id).innerHTML = "";
+            document.getElementById(t[m].id).innerHTML = "";
         }
-        while (true) {
-            if (j == data.alldata[2].length) {
-                break;
+    }
+    while (true) {
+        if (j == data.alldata[2].length) {
+            break;
+        }
+        if (x[j].id == data.alldata[0][i].tower_name) {
+            if (data.alldata[0][i].mode == "mp") {
+                if (data.alldata[0][i].ping == "Request timeout") {
+                    document.getElementById(x[j].id).innerHTML += '<div class="nodes"><i class="fa fa-circle" style="background: -webkit-radial-gradient(#ff3000, #ff7800); -webkit-background-clip: text; -webkit-text-fill-color: transparent;""></i><div class="tooltip">' + data.alldata[0][i].device_name + '<span class="tooltiptext" style="top: -39px;">APN: ' + data.alldata[0][i].device_name + ' </br> Ping: ' + data.alldata[0][i].ping + '</br> IP: ' + data.alldata[0][i].ip + '</br>Radio: ' + data.alldata[0][i].models + '</br>OS: ' + data.alldata[0][i].os + '</br>Status: ' + data.alldata[0][i].status + '</span></div></div>';
+                    i++;
+                } else {
+                    document.getElementById(x[j].id).innerHTML += '<div class="nodes"><i class="fa fa-circle" style="background: -webkit-radial-gradient(#60e555, #60e555); -webkit-background-clip: text; -webkit-text-fill-color: transparent;"></i><div class="tooltip">' + data.alldata[0][i].device_name + '<span class="tooltiptext" style="top: -39px;">APN: ' + data.alldata[0][i].device_name + ' </br> Ping: ' + data.alldata[0][i].ping + '</br> IP: ' + data.alldata[0][i].ip + '</br>Radio: ' + data.alldata[0][i].models + '</br>OS: ' + data.alldata[0][i].os + '</br>Status: Online</span></div></div>';
+                    i++;
+                    }
+            } else {
+                //pass
             }
-            if (x[j].id == data.alldata[0][i].tower_name) {
-                if (data.alldata[0][i].ptp == "0") {
+            if (data.alldata[0][i].mode == "ptp") {
+                if (data.alldata[0][i].ping == "Request timeout") {
+                    document.getElementById(x[j].id).innerHTML += '<div class="nodes"><i class="fa fa-arrow-circle-up" style="background: -webkit-radial-gradient(#ff3000, #ff7800); -webkit-background-clip: text; -webkit-text-fill-color: transparent;""></i><div class="tooltip">' + data.alldata[0][i].device_name + '<span class="tooltiptext" style="top: -39px;">APN: ' + data.alldata[0][i].device_name + ' </br> Ping: ' + data.alldata[0][i].ping + '</br> IP: ' + data.alldata[0][i].ip + '</br>Radio: ' + data.alldata[0][i].models + '</br>OS: ' + data.alldata[0][i].os + '</br>Status: ' + data.alldata[0][i].status + '</span></div></div>';
+                    i++;
+                } else {
+                        document.getElementById(x[j].id).innerHTML += '<div class="nodes"><i class="fa fa-arrow-circle-up" style="background: -webkit-radial-gradient(#60e555, #60e555); -webkit-background-clip: text; -webkit-text-fill-color: transparent;"></i><div class="tooltip">' + data.alldata[0][i].device_name + '<span class="tooltiptext" style="top: -39px;">APN: ' + data.alldata[0][i].device_name + ' </br> Ping: ' + data.alldata[0][i].ping + '</br> IP: ' + data.alldata[0][i].ip + '</br>Radio: ' + data.alldata[0][i].models + '</br>OS: ' + data.alldata[0][i].os + '</br>Status: Online</span></div></div>';
+                        i++;
+                }
+            } else {
+                //pass
+            }
+            if (data.alldata[0][i].mode == "ptpt") {
+                if ("ups" + data.alldata[0][i].tower_name == t[j].id) {
                     if (data.alldata[0][i].ping == "Request timeout") {
-                        document.getElementById(x[j].id).innerHTML += '<div class="nodes"><i class="fa fa-circle" style="background: -webkit-radial-gradient(#ff3000, #ff7800); -webkit-background-clip: text; -webkit-text-fill-color: transparent;""></i><div class="tooltip">' + data.alldata[0][i].device_name + '<span class="tooltiptext" style="top: -25px;">APN: ' + data.alldata[0][i].device_name + ' </br> Ping: ' + data.alldata[0][i].ping + '</br> IP: <a class="copy" onclick="copy(this)">' + data.alldata[0][i].ip + '</a></br>Model: ' + data.alldata[0][i].models + '</br>OS: ' + data.alldata[0][i].OS + '</span></div></div>';
+                        document.getElementById(t[j].id).innerHTML += '<div class="tooltip"><i class="fa fa-link" aria-hidden="true" style="background: -webkit-radial-gradient(#ff3000, #ff7800); -webkit-background-clip: text; -webkit-text-fill-color: transparent;"></i><span class="tooltiptext" style="top: -20px; left: 25px;">Ping: ' + data.alldata[0][i].ping + '</br>IP:' + data.alldata[0][i].ip + '</br>Model: ' + data.alldata[0][i].models + '</br>OS: ' + data.alldata[0][i].os + '</br>Status: ' + data.alldata[0][i].status + '</span></div></br>';
                         i++;
                     } else {
-                        document.getElementById(x[j].id).innerHTML += '<div class="nodes"><i class="fa fa-circle" style="background: -webkit-radial-gradient(#60e555, #60e555); -webkit-background-clip: text; -webkit-text-fill-color: transparent;"></i><div class="tooltip">' + data.alldata[0][i].device_name + '<span class="tooltiptext" style="top: -25px;">APN: ' + data.alldata[0][i].device_name + ' </br> Ping: ' + data.alldata[0][i].ping + '</br> IP: <a class="copy" onclick="copy(this)">' + data.alldata[0][i].ip + '</a></br>Model: ' + data.alldata[0][i].models + '</br>OS: ' + data.alldata[0][i].OS + '</span></div></div>';
+                        document.getElementById(t[j].id).innerHTML += '<div class="tooltip"><i class="fa fa-link" aria-hidden="true" style="background: -webkit-radial-gradient(#60e555, #60e555); -webkit-background-clip: text; -webkit-text-fill-color: transparent;"></i><span class="tooltiptext" style="top: -20px; left: 25px;">Ping: ' + data.alldata[0][i].ping + '</br> IP: ' + data.alldata[0][i].ip + '</br>Model: ' + data.alldata[0][i].models + '</br>OS: ' + data.alldata[0][i].os + '</br>Status: Online</span></div></br>';
                         i++;
-                        }
+                    }
                 }
-                if (data.alldata[0][i].ptp == "1") {
+            } else {
+                //pass
+            }
+            if (data.alldata[0][i].mode == "router") {
+                if ("ups" + data.alldata[0][i].tower_name == t[j].id) {
                     if (data.alldata[0][i].ping == "Request timeout") {
-                        document.getElementById(x[j].id).innerHTML += '<div class="nodes"><i class="fa fa-arrow-circle-up" style="background: -webkit-radial-gradient(#ff3000, #ff7800); -webkit-background-clip: text; -webkit-text-fill-color: transparent;""></i><div class="tooltip">' + data.alldata[0][i].device_name + '<span class="tooltiptext" style="top: -25px;">APN: ' + data.alldata[0][i].device_name + ' </br> Ping: ' + data.alldata[0][i].ping + '</br> IP: <a class="copy" onclick="copy(this)">' + data.alldata[0][i].ip + '</a></br>Model: ' + data.alldata[0][i].models + '</br>OS: ' + data.alldata[0][i].OS + '</span></div></div>';
+                        document.getElementById(t[j].id).innerHTML += '<div class="tooltip"><i class="fa fa-arrows-alt" aria-hidden="true" style="background: -webkit-radial-gradient(#ff3000, #ff7800); -webkit-background-clip: text; -webkit-text-fill-color: transparent;"></i><span class="tooltiptext" style="top: -20px; left: 25px;">Ping: ' + data.alldata[0][i].ping + '</br> IP: ' + data.alldata[0][i].ip + '</br>Model: ' + data.alldata[0][i].models + '</br>OS: ' + data.alldata[0][i].os + '</br>Status: ' + data.alldata[0][i].status + '</span></div></br>';
                         i++;
                     } else {
-                            document.getElementById(x[j].id).innerHTML += '<div class="nodes"><i class="fa fa-arrow-circle-up" style="background: -webkit-radial-gradient(#60e555, #60e555); -webkit-background-clip: text; -webkit-text-fill-color: transparent;"></i><div class="tooltip">' + data.alldata[0][i].device_name + '<span class="tooltiptext" style="top: -25px;">APN: ' + data.alldata[0][i].device_name + ' </br> Ping: ' + data.alldata[0][i].ping + '</br> IP: <a class="copy" onclick="copy(this)">' + data.alldata[0][i].ip + '</a></br>Model: ' + data.alldata[0][i].models + '</br>OS: ' + data.alldata[0][i].OS + '</span></div></div>';
-                            i++;
-                    }
-                }
-                if (data.alldata[0][i].ptp == "4") {
-                    if ("ups" + data.alldata[0][i].tower_name == t[j].id) {
-                        if (data.alldata[0][i].ping == "Request timeout") {
-                            document.getElementById(t[j].id).innerHTML += '<div class="tooltip"><i class="fa fa-link" aria-hidden="true" style="background: -webkit-radial-gradient(#ff3000, #ff7800); -webkit-background-clip: text; -webkit-text-fill-color: transparent;"></i><span class="tooltiptext" style="top: 0px; left: 25px;">Ping: ' + data.alldata[0][i].ping + '</br>OS: ' + data.alldata[0][i].os + '</br>Model: ' + data.alldata[0][i].models + '</br> IP: <a onclick="copy(this)">' + data.alldata[0][i].ip + '</a></span></div></br>';
-                            i++;
-                        } else {
-                            document.getElementById(t[j].id).innerHTML += '<div class="tooltip"><i class="fa fa-link" aria-hidden="true" style="background: -webkit-radial-gradient(#60e555, #60e555); -webkit-background-clip: text; -webkit-text-fill-color: transparent;"></i><span class="tooltiptext" style="top: 0px; left: 25px;">Ping: ' + data.alldata[0][i].ping + '</br>OS: ' + data.alldata[0][i].os + '</br>Model: ' + data.alldata[0][i].models + '</br> IP: <a onclick="copy(this)">' + data.alldata[0][i].ip + '</a></span></div></br>';
-                            i++;
-                        }
-                    }
-                }
-                if (data.alldata[0][i].ptp == "3") {
-                    if ("ups" + data.alldata[0][i].tower_name == t[j].id) {
-                        if (data.alldata[0][i].ping == "Request timeout") {
-                            document.getElementById(t[j].id).innerHTML += '<div class="tooltip"><i class="fa fa-arrows-alt" aria-hidden="true" style="background: -webkit-radial-gradient(#ff3000, #ff7800); -webkit-background-clip: text; -webkit-text-fill-color: transparent;"></i><span class="tooltiptext" style="top: 0px; left: 25px;">Ping: ' + data.alldata[0][i].ping + '</br> IP: <a class="copy" onclick="copy(this)">' + data.alldata[0][i].ip + '</a></span></div></br>';
-                            i++;
-                        } else {
-                            document.getElementById(t[j].id).innerHTML += '<div class="tooltip"><i class="fa fa-arrows-alt" aria-hidden="true" style="background: -webkit-radial-gradient(#60e555, #60e555); -webkit-background-clip: text; -webkit-text-fill-color: transparent;"></i><span class="tooltiptext" style="top: 0px; left: 25px;">Ping: ' + data.alldata[0][i].ping + '</br> IP: <a class="copy" onclick="copy(this)">' + data.alldata[0][i].ip + '</a></span></div></br>';
-                            i++;
-                        }
-                    }
-                }
-                if (data.alldata[0][i].ptp == "2") {
-                    if ("ups" + data.alldata[0][i].tower_name == t[j].id) {
-                        if (data.alldata[0][i].ping == "Request timeout") {
-                            document.getElementById(t[j].id).innerHTML += '<div class="tooltip"><i class="fa fa-battery-full" aria-hidden="true" style="background: -webkit-radial-gradient(#ff3000, #ff7800); -webkit-background-clip: text; -webkit-text-fill-color: transparent;"></i><span class="tooltiptext" style="top: 0px; left: 25px;">Ping: ' + data.alldata[0][i].ping + '</br> IP: <a class="copy" onclick="copy(this)">' + data.alldata[0][i].ip + '</a></span></div></br>';
-                            i++;
-                        } else {
-                            document.getElementById(t[j].id).innerHTML += '<div class="tooltip"><i class="fa fa-battery-full" aria-hidden="true" style="background: -webkit-radial-gradient(#60e555, #60e555); -webkit-background-clip: text; -webkit-text-fill-color: transparent;"></i><span class="tooltiptext" style="top: 0px; left: 25px;">Ping: ' + data.alldata[0][i].ping + '</br> IP: <a class="copy" onclick="copy(this)">' + data.alldata[0][i].ip + '</a></span></div></br>';
-                            i++;
-                        }
+                        document.getElementById(t[j].id).innerHTML += '<div class="tooltip"><i class="fa fa-arrows-alt" aria-hidden="true" style="background: -webkit-radial-gradient(#60e555, #60e555); -webkit-background-clip: text; -webkit-text-fill-color: transparent;"></i><span class="tooltiptext" style="top: -20px; left: 25px;">Ping: ' + data.alldata[0][i].ping + '</br> IP: ' + data.alldata[0][i].ip + '</br>Model: ' + data.alldata[0][i].models + '</br>OS: ' + data.alldata[0][i].os + '</br>Status: Online</span></div></br>';
+                        i++;
                     }
                 }
             } else {
-                i++;
+                //pass
             }
-            if (j == data.alldata[2].length) {
-                break;
-            } else {
-                if (data.alldata[0].length == i) {
-                    j++;
-                    i = 0;
+            if (data.alldata[0][i].mode == "ups") {
+                if ("ups" + data.alldata[0][i].tower_name == t[j].id) {
+                    if (data.alldata[0][i].ping == "Request timeout") {
+                        document.getElementById(t[j].id).innerHTML += '<div class="tooltip"><i class="fa fa-battery-full" aria-hidden="true" style="background: -webkit-radial-gradient(#ff3000, #ff7800); -webkit-background-clip: text; -webkit-text-fill-color: transparent;"></i><span class="tooltiptext" style="top: -5px; left: 25px;">Ping: ' + data.alldata[0][i].ping + '</br> IP: ' + data.alldata[0][i].ip + '</br>Status: ' + data.alldata[0][i].status + '</span></div></br>';
+                        i++;
+                    } else {
+                        document.getElementById(t[j].id).innerHTML += '<div class="tooltip"><i class="fa fa-battery-full" aria-hidden="true" style="background: -webkit-radial-gradient(#60e555, #60e555); -webkit-background-clip: text; -webkit-text-fill-color: transparent;"></i><span class="tooltiptext" style="top: -5px; left: 25px;">Ping: ' + data.alldata[0][i].ping + '</br> IP: ' + data.alldata[0][i].ip + '</br>Status: Online</span></div></br>';
+                        i++;
+                    }
                 }
+            } else {
+                //pass
+            }
+        } else {
+            i++;
+        }
+        if (j == data.alldata[2].length) {
+            break;
+        } else {
+            if (data.alldata[0].length == i) {
+            j++;
+            i = 0;
             }
         }
-});
-}
+    }
+});}
 
-function screen() {
-    $.getJSON($SCRIPT_ROOT + '/_stuff', function(data) {
+function screen() { $.getJSON($SCRIPT_ROOT + '/_stuff',
+    function(data) {
         for (i = 0; i < data.alldata[2].length; i++) {
-            document.getElementById('containment-wrapper').innerHTML += '<div id="' + data.alldata[2][i].id +'" class="draggable" data-left="20px" data-top="20px" style="position: absolute; left:' + data.alldata[2][i].left + '; top:' + data.alldata[2][i].top + '"><div class="left"><div class="left_top"><a>' + data.alldata[2][i].tower_name + '</a><br><div class="tooltip"><i class="fa fa-info-circle fa-1x" aria-hidden="true"></i><span class="tooltiptext" style="top: -22px; left: 25px;">Name: ' + data.alldata[2][i].address + '</span></div></div></br><div class="left_down" id="ups' + data.alldata[2][i].tower_name + '"></div></div><div class="right" id="' + data.alldata[2][i].tower_name + '"></div></div>';
+            document.getElementById('containment-wrapper').innerHTML += '<div id="1" class="draggable" data-left="20px" data-top="20px" style="position: absolute; left:' + data.alldata[2][i].left + '; top:' + data.alldata[2][i].top + '"><div class="left"><div class="left_top"><a>' + data.alldata[2][i].tower_name + '</a><br><i style="--fa-animation-duration: 20s; color: black;" class="fa-solid fa-cog fa-spin"></i></div></br><div class="left_down" id="ups' + data.alldata[2][i].tower_name + '"></div></div><div class="right" id="' + data.alldata[2][i].tower_name + '"></div></div>';
         }
         t = document.getElementsByClassName("left_down");
-    });
-}
+    });}
