f8a724cdaf9b434c83176599c52690c061013744
Admiral <91305229+pooyatavan@users.noreply.github.com>
2022-04-18 12:45:13 +0430

    Update solar.js

--- static/js/solar.js
+++ static/js/solar.js
@@ -16,6 +16,7 @@ function update_values() {$.getJSON($SCRIPT_ROOT + '/_stuff', function(data) {
             document.getElementById(t[m].id).innerHTML = "";
         }
     }
+
     while (true) {
         if (j == data.alldata[2].length) {
             break;
@@ -23,7 +24,7 @@ function update_values() {$.getJSON($SCRIPT_ROOT + '/_stuff', function(data) {
         if (x[j].id == data.alldata[0][i].tower_name) {
             if (data.alldata[0][i].mode == "mp") {
                 if (data.alldata[0][i].status == "enable") {
-                    if (data.alldata[0][i].ping == "Request timeout") {
+                    if (data.alldata[0][i].ping == "Offline") {
                         document.getElementById(x[j].id).innerHTML += '<div class="nodes"><i class="fa fa-circle" style="background: -webkit-radial-gradient(#ff3000, #ff7800); -webkit-background-clip: text; -webkit-text-fill-color: transparent;""></i><div class="tooltip">' + data.alldata[0][i].device_name + '<span class="tooltiptext" style="top: -39px;">APN: ' + data.alldata[0][i].device_name + ' </br> Ping: ' + data.alldata[0][i].ping + '</br> IP: ' + data.alldata[0][i].ip + '</br>Radio: ' + data.alldata[0][i].models + '</br>OS: ' + data.alldata[0][i].os + '</br>Status: ' + data.alldata[0][i].status + '</span></div></div>';
                         i++;
                     } else {
@@ -34,12 +35,11 @@ function update_values() {$.getJSON($SCRIPT_ROOT + '/_stuff', function(data) {
                     document.getElementById(x[j].id).innerHTML += '<div class="nodes"><i class="fa fa-circle" style="background: -webkit-radial-gradient(#1E90FF, #1E90FF); -webkit-background-clip: text; -webkit-text-fill-color: transparent;""></i><div class="tooltip">' + data.alldata[0][i].device_name + '<span class="tooltiptext" style="top: -39px;">APN: ' + data.alldata[0][i].device_name + ' </br> Ping: Disable</br> IP: ' + data.alldata[0][i].ip + '</br>Radio: ' + data.alldata[0][i].models + '</br>OS: ' + data.alldata[0][i].os + '</br>Status: ' + data.alldata[0][i].status + '</span></div></div>';
                     i++;
                 }
-            } else {
-                //pass
             }
+
             if (data.alldata[0][i].mode == "ptp") {
                 if (data.alldata[0][i].status == "enable") {
-                    if (data.alldata[0][i].ping == "Request timeout") {
+                    if (data.alldata[0][i].ping == "Offline") {
                         document.getElementById(x[j].id).innerHTML += '<div class="nodes"><i class="fa fa-arrow-circle-up" style="background: -webkit-radial-gradient(#ff3000, #ff7800); -webkit-background-clip: text; -webkit-text-fill-color: transparent;""></i><div class="tooltip">' + data.alldata[0][i].device_name + '<span class="tooltiptext" style="top: -39px;">APN: ' + data.alldata[0][i].device_name + ' </br> Ping: ' + data.alldata[0][i].ping + '</br> IP: ' + data.alldata[0][i].ip + '</br>Radio: ' + data.alldata[0][i].models + '</br>OS: ' + data.alldata[0][i].os + '</br>Status: ' + data.alldata[0][i].status + '</span></div></div>';
                         i++;
                     } else {
@@ -50,13 +50,12 @@ function update_values() {$.getJSON($SCRIPT_ROOT + '/_stuff', function(data) {
                     document.getElementById(x[j].id).innerHTML += '<div class="nodes"><i class="fa fa-arrow-circle-up" style="background: -webkit-radial-gradient(#1E90FF, #1E90FF); -webkit-background-clip: text; -webkit-text-fill-color: transparent;"></i><div class="tooltip">' + data.alldata[0][i].device_name + '<span class="tooltiptext" style="top: -39px;">APN: ' + data.alldata[0][i].device_name + ' </br> Ping: Disabel</br> IP: ' + data.alldata[0][i].ip + '</br>Radio: ' + data.alldata[0][i].models + '</br>OS: ' + data.alldata[0][i].os + '</br>Status: Online</span></div></div>';
                     i++;
                 }
-            } else {
-                //pass
             }
+
             if (data.alldata[0][i].mode == "ptpt") {
                 if ("ups" + data.alldata[0][i].tower_name == t[j].id) {
                     if (data.alldata[0][i].status == "enable") {
-                        if (data.alldata[0][i].ping == "Request timeout") {
+                        if (data.alldata[0][i].ping == "Offline") {
                             document.getElementById(t[j].id).innerHTML += '<div class="tooltip"><i class="fa fa-link" aria-hidden="true" style="background: -webkit-radial-gradient(#ff3000, #ff7800); -webkit-background-clip: text; -webkit-text-fill-color: transparent;"></i><span class="tooltiptext" style="top: -20px; left: 25px;">Ping: ' + data.alldata[0][i].ping + '</br>IP:' + data.alldata[0][i].ip + '</br>Model: ' + data.alldata[0][i].models + '</br>OS: ' + data.alldata[0][i].os + '</br>Status: ' + data.alldata[0][i].status + '</span></div></br>';
                             i++;
                         } else {
@@ -68,13 +67,11 @@ function update_values() {$.getJSON($SCRIPT_ROOT + '/_stuff', function(data) {
                         i++; 
                     }
                 }
-            } else {
-                //pass
             }
             
             if (data.alldata[0][i].mode == "router") {
                 if ("ups" + data.alldata[0][i].tower_name == t[j].id) {
-                    if (data.alldata[0][i].ping == "Request timeout") {
+                    if (data.alldata[0][i].ping == "Offline") {
                         document.getElementById(t[j].id).innerHTML += '<div class="tooltip"><i class="fa fa-arrows-alt" aria-hidden="true" style="background: -webkit-radial-gradient(#ff3000, #ff7800); -webkit-background-clip: text; -webkit-text-fill-color: transparent;"></i><span class="tooltiptext" style="top: -20px; left: 25px;">Ping: ' + data.alldata[0][i].ping + '</br> IP: ' + data.alldata[0][i].ip + '</br>Model: ' + data.alldata[0][i].models + '</br>OS: ' + data.alldata[0][i].os + '</br>Status: ' + data.alldata[0][i].status + '</span></div></br>';
                         i++;
                     } else {
@@ -82,12 +79,11 @@ function update_values() {$.getJSON($SCRIPT_ROOT + '/_stuff', function(data) {
                         i++;
                     }
                 }
-            } else {
-                //pass
             }
+
             if (data.alldata[0][i].mode == "ups") {
                 if ("ups" + data.alldata[0][i].tower_name == t[j].id) {
-                    if (data.alldata[0][i].ping == "Request timeout") {
+                    if (data.alldata[0][i].ping == "Offline") {
                         document.getElementById(t[j].id).innerHTML += '<div class="tooltip"><i class="fa fa-battery-full" aria-hidden="true" style="background: -webkit-radial-gradient(#ff3000, #ff7800); -webkit-background-clip: text; -webkit-text-fill-color: transparent;"></i><span class="tooltiptext" style="top: -5px; left: 25px;">Ping: ' + data.alldata[0][i].ping + '</br> IP: ' + data.alldata[0][i].ip + '</br>Status: ' + data.alldata[0][i].status + '</span></div></br>';
                         i++;
                     } else {
@@ -95,12 +91,11 @@ function update_values() {$.getJSON($SCRIPT_ROOT + '/_stuff', function(data) {
                         i++;
                     }
                 }
-            } else {
-                //pass
             }
-        } else {
-            i++;
-        }
+
+    } else {
+        i++;
+    }
         if (j == data.alldata[2].length) {
             break;
         } else {
