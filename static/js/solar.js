fa47513386acc558ec0e0d352c3432ca7fb78411
Admiral <91305229+pooyatavan@users.noreply.github.com>
2022-04-20 20:27:34 +0430

    Fix
    Active

--- static/js/solar.js
+++ static/js/solar.js
@@ -24,8 +24,8 @@ function update_values() {$.getJSON($SCRIPT_ROOT + '/_stuff', function(data) {
         if (x[j].id == data.alldata[0][i].tower_name) {
             if (data.alldata[0][i].mode == "mp") {
                 if (data.alldata[0][i].status == "enable") {
-                    if (data.alldata[0][i].ping == "Offline") {
-                        document.getElementById(x[j].id).innerHTML += '<div class="nodes"><i class="fa fa-circle" style="background: -webkit-radial-gradient(#ff3000, #ff7800); -webkit-background-clip: text; -webkit-text-fill-color: transparent;""></i><div class="tooltip">' + data.alldata[0][i].device_name + '<span class="tooltiptext" style="top: -39px;">APN: ' + data.alldata[0][i].device_name + ' </br> Ping: ' + data.alldata[0][i].ping + '</br> IP: ' + data.alldata[0][i].ip + '</br>Radio: ' + data.alldata[0][i].models + '</br>OS: ' + data.alldata[0][i].os + '</br>Status: ' + data.alldata[0][i].status + '</span></div></div>';
+                    if (data.alldata[0][i].ping == "Request timeout") {
+                        document.getElementById(x[j].id).innerHTML += '<div class="nodes"><i class="fa fa-circle" style="background: -webkit-radial-gradient(#ff3000, #ff7800); -webkit-background-clip: text; -webkit-text-fill-color: transparent;""></i><div class="tooltip">' + data.alldata[0][i].device_name + '<span class="tooltiptext" style="top: -45px;">APN: ' + data.alldata[0][i].device_name + ' </br> Ping: ' + data.alldata[0][i].ping + '</br> IP: ' + data.alldata[0][i].ip + '</br>Radio: ' + data.alldata[0][i].models + '</br>OS: ' + data.alldata[0][i].os + '</br>Status: ' + data.alldata[0][i].status + '</br>Active: ' + data.alldata[0][i].time_active + '</span></div></div>';
                         i++;
                     } else {
                         document.getElementById(x[j].id).innerHTML += '<div class="nodes"><i class="fa fa-circle" style="background: -webkit-radial-gradient(#60e555, #60e555); -webkit-background-clip: text; -webkit-text-fill-color: transparent;"></i><div class="tooltip">' + data.alldata[0][i].device_name + '<span class="tooltiptext" style="top: -39px;">APN: ' + data.alldata[0][i].device_name + ' </br> Ping: ' + data.alldata[0][i].ping + '</br> IP: ' + data.alldata[0][i].ip + '</br>Radio: ' + data.alldata[0][i].models + '</br>OS: ' + data.alldata[0][i].os + '</br>Status: Online</span></div></div>';
@@ -39,8 +39,8 @@ function update_values() {$.getJSON($SCRIPT_ROOT + '/_stuff', function(data) {
 
             if (data.alldata[0][i].mode == "ptp") {
                 if (data.alldata[0][i].status == "enable") {
-                    if (data.alldata[0][i].ping == "Offline") {
-                        document.getElementById(x[j].id).innerHTML += '<div class="nodes"><i class="fa fa-arrow-circle-up" style="background: -webkit-radial-gradient(#ff3000, #ff7800); -webkit-background-clip: text; -webkit-text-fill-color: transparent;""></i><div class="tooltip">' + data.alldata[0][i].device_name + '<span class="tooltiptext" style="top: -39px;">APN: ' + data.alldata[0][i].device_name + ' </br> Ping: ' + data.alldata[0][i].ping + '</br> IP: ' + data.alldata[0][i].ip + '</br>Radio: ' + data.alldata[0][i].models + '</br>OS: ' + data.alldata[0][i].os + '</br>Status: ' + data.alldata[0][i].status + '</span></div></div>';
+                    if (data.alldata[0][i].ping == "Request timeout") {
+                        document.getElementById(x[j].id).innerHTML += '<div class="nodes"><i class="fa fa-arrow-circle-up" style="background: -webkit-radial-gradient(#ff3000, #ff7800); -webkit-background-clip: text; -webkit-text-fill-color: transparent;""></i><div class="tooltip">' + data.alldata[0][i].device_name + '<span class="tooltiptext" style="top: -45px;">APN: ' + data.alldata[0][i].device_name + ' </br> Ping: ' + data.alldata[0][i].ping + '</br> IP: ' + data.alldata[0][i].ip + '</br>Radio: ' + data.alldata[0][i].models + '</br>OS: ' + data.alldata[0][i].os + '</br>Status: ' + data.alldata[0][i].status + '</br>Active: ' + data.alldata[0][i].time_active + '</span></div></div>';
                         i++;
                     } else {
                         document.getElementById(x[j].id).innerHTML += '<div class="nodes"><i class="fa fa-arrow-circle-up" style="background: -webkit-radial-gradient(#60e555, #60e555); -webkit-background-clip: text; -webkit-text-fill-color: transparent;"></i><div class="tooltip">' + data.alldata[0][i].device_name + '<span class="tooltiptext" style="top: -39px;">APN: ' + data.alldata[0][i].device_name + ' </br> Ping: ' + data.alldata[0][i].ping + '</br> IP: ' + data.alldata[0][i].ip + '</br>Radio: ' + data.alldata[0][i].models + '</br>OS: ' + data.alldata[0][i].os + '</br>Status: Online</span></div></div>';
@@ -55,8 +55,8 @@ function update_values() {$.getJSON($SCRIPT_ROOT + '/_stuff', function(data) {
             if (data.alldata[0][i].mode == "ptpt") {
                 if ("ups" + data.alldata[0][i].tower_name == t[j].id) {
                     if (data.alldata[0][i].status == "enable") {
-                        if (data.alldata[0][i].ping == "Offline") {
-                            document.getElementById(t[j].id).innerHTML += '<div class="tooltip"><i class="fa fa-link" aria-hidden="true" style="background: -webkit-radial-gradient(#ff3000, #ff7800); -webkit-background-clip: text; -webkit-text-fill-color: transparent;"></i><span class="tooltiptext" style="top: -20px; left: 25px;">Ping: ' + data.alldata[0][i].ping + '</br>IP:' + data.alldata[0][i].ip + '</br>Model: ' + data.alldata[0][i].models + '</br>OS: ' + data.alldata[0][i].os + '</br>Status: ' + data.alldata[0][i].status + '</span></div></br>';
+                        if (data.alldata[0][i].ping == "Request timeout") {
+                            document.getElementById(t[j].id).innerHTML += '<div class="tooltip"><i class="fa fa-link" aria-hidden="true" style="background: -webkit-radial-gradient(#ff3000, #ff7800); -webkit-background-clip: text; -webkit-text-fill-color: transparent;"></i><span class="tooltiptext" style="top: -27px; left: 25px;">Ping: ' + data.alldata[0][i].ping + '</br>IP:' + data.alldata[0][i].ip + '</br>Model: ' + data.alldata[0][i].models + '</br>OS: ' + data.alldata[0][i].os + '</br>Status: ' + data.alldata[0][i].status + '</br>Active: ' + data.alldata[0][i].time_active + '</span></div></br>';
                             i++;
                         } else {
                             document.getElementById(t[j].id).innerHTML += '<div class="tooltip"><i class="fa fa-link" aria-hidden="true" style="background: -webkit-radial-gradient(#60e555, #60e555); -webkit-background-clip: text; -webkit-text-fill-color: transparent;"></i><span class="tooltiptext" style="top: -20px; left: 25px;">Ping: ' + data.alldata[0][i].ping + '</br> IP: ' + data.alldata[0][i].ip + '</br>Model: ' + data.alldata[0][i].models + '</br>OS: ' + data.alldata[0][i].os + '</br>Status: Online</span></div></br>';
@@ -71,8 +71,8 @@ function update_values() {$.getJSON($SCRIPT_ROOT + '/_stuff', function(data) {
             
             if (data.alldata[0][i].mode == "router") {
                 if ("ups" + data.alldata[0][i].tower_name == t[j].id) {
-                    if (data.alldata[0][i].ping == "Offline") {
-                        document.getElementById(t[j].id).innerHTML += '<div class="tooltip"><i class="fa fa-arrows-alt" aria-hidden="true" style="background: -webkit-radial-gradient(#ff3000, #ff7800); -webkit-background-clip: text; -webkit-text-fill-color: transparent;"></i><span class="tooltiptext" style="top: -20px; left: 25px;">Ping: ' + data.alldata[0][i].ping + '</br> IP: ' + data.alldata[0][i].ip + '</br>Model: ' + data.alldata[0][i].models + '</br>OS: ' + data.alldata[0][i].os + '</br>Status: ' + data.alldata[0][i].status + '</span></div></br>';
+                    if (data.alldata[0][i].ping == "Request timeout") {
+                        document.getElementById(t[j].id).innerHTML += '<div class="tooltip"><i class="fa fa-arrows-alt" aria-hidden="true" style="background: -webkit-radial-gradient(#ff3000, #ff7800); -webkit-background-clip: text; -webkit-text-fill-color: transparent;"></i><span class="tooltiptext" style="top: -20px; left: 25px;">Ping: ' + data.alldata[0][i].ping + '</br> IP: ' + data.alldata[0][i].ip + '</br>Model: ' + data.alldata[0][i].models + '</br>OS: ' + data.alldata[0][i].os + '</br>Status: ' + data.alldata[0][i].status + '</br>Active: ' + data.alldata[0][i].time_active + '</span></div></br>';
                         i++;
                     } else {
                         document.getElementById(t[j].id).innerHTML += '<div class="tooltip"><i class="fa fa-arrows-alt" aria-hidden="true" style="background: -webkit-radial-gradient(#60e555, #60e555); -webkit-background-clip: text; -webkit-text-fill-color: transparent;"></i><span class="tooltiptext" style="top: -20px; left: 25px;">Ping: ' + data.alldata[0][i].ping + '</br> IP: ' + data.alldata[0][i].ip + '</br>Model: ' + data.alldata[0][i].models + '</br>OS: ' + data.alldata[0][i].os + '</br>Status: Online</span></div></br>';
@@ -83,8 +83,8 @@ function update_values() {$.getJSON($SCRIPT_ROOT + '/_stuff', function(data) {
 
             if (data.alldata[0][i].mode == "ups") {
                 if ("ups" + data.alldata[0][i].tower_name == t[j].id) {
-                    if (data.alldata[0][i].ping == "Offline") {
-                        document.getElementById(t[j].id).innerHTML += '<div class="tooltip"><i class="fa fa-battery-full" aria-hidden="true" style="background: -webkit-radial-gradient(#ff3000, #ff7800); -webkit-background-clip: text; -webkit-text-fill-color: transparent;"></i><span class="tooltiptext" style="top: -5px; left: 25px;">Ping: ' + data.alldata[0][i].ping + '</br> IP: ' + data.alldata[0][i].ip + '</br>Status: ' + data.alldata[0][i].status + '</span></div></br>';
+                    if (data.alldata[0][i].ping == "Request timeout") {
+                        document.getElementById(t[j].id).innerHTML += '<div class="tooltip"><i class="fa fa-battery-full" aria-hidden="true" style="background: -webkit-radial-gradient(#ff3000, #ff7800); -webkit-background-clip: text; -webkit-text-fill-color: transparent;"></i><span class="tooltiptext" style="top: -5px; left: 25px;">Ping: ' + data.alldata[0][i].ping + '</br> IP: ' + data.alldata[0][i].ip + '</br>Status: ' + data.alldata[0][i].status + '</br>Active: ' + data.alldata[0][i].time_active + '</span></div></br>';
                         i++;
                     } else {
                         document.getElementById(t[j].id).innerHTML += '<div class="tooltip"><i class="fa fa-battery-full" aria-hidden="true" style="background: -webkit-radial-gradient(#60e555, #60e555); -webkit-background-clip: text; -webkit-text-fill-color: transparent;"></i><span class="tooltiptext" style="top: -5px; left: 25px;">Ping: ' + data.alldata[0][i].ping + '</br> IP: ' + data.alldata[0][i].ip + '</br>Status: Online</span></div></br>';
